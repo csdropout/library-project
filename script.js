d526e2f7ca2f4ca415dfc6115deced0744986162
qile <98936712+csdropout@users.noreply.github.com>
2026-03-03 16:27:13 +0800

    Refactor book card creation

--- script.js
+++ script.js
@@ -23,61 +23,8 @@ function displayBooks() {
 
     // loop through library
     for (const book of myLibrary) {
-        // create card
-        const bookCard = document.createElement("div");
-        bookCard.setAttribute("class", "card")
-
-        // create p element for title
-        const title = document.createElement("p");
-        title.textContent = `Title: ${book.title}`;
-
-        // create p element for author
-        const author = document.createElement("p");
-        author.textContent = `Author: ${book.author}`;
-
-        // create p element for book id
-        const bookId = document.createElement("p");
-        bookId.textContent = `ID: ${book.id}`;
-
-        // create element for read status
-        const readStatus = document.createElement("p");
-        readStatus.textContent = `Status: ${book.hasRead? "Read" : "Not read"}`;
-
-        // Add a button on each book’s display to remove the book from the library.
-        // create delete button
-        const deleteButton = document.createElement("button");
-        deleteButton.textContent = "Delete";
-        deleteButton.addEventListener("click", function(e) {
-            // remove book from library array
-            myLibrary = myLibrary.filter(function (libraryBook) {
-                return libraryBook.id !== book.id;
-            })
-
-            // remove book from display
-            this.parentElement.remove();
-        })
-
-        // Add a button on each book’s display to change its read status.
-        const readButton = document.createElement("button");
-        readButton.textContent = "Change read status";
-        readButton.addEventListener("click", () => {
-            book.toggleReadStatus();
-            readStatus.textContent = `Status: ${book.hasRead? "Read" : "Not read"}`;
-        })
-
-        // group buttons
-        const cardButtons = document.createElement("div");
-        cardButtons.setAttribute("class", "card-buttons");
-        cardButtons.appendChild(readButton);
-        cardButtons.appendChild(deleteButton);
-
-        // add elements to card
-        bookCard.appendChild(title);
-        bookCard.appendChild(author);
-        bookCard.appendChild(bookId);
-        bookCard.appendChild(readStatus);
-        bookCard.appendChild(cardButtons);
-
+        // create book card
+        const bookCard = createBookCard(book)
         // append card to container
         booksContainer.appendChild(bookCard);
     }
@@ -98,8 +45,66 @@ form.addEventListener("submit", function addBook(event) {
     const title = book['title'];
     const author = book['author'];
     newBook = new Book(title, author);
-
     myLibrary.push(newBook);
 
     newBookDialog.hidePopover();
 })
+
+function createBookCard(book) {
+    // create card
+    const bookCard = document.createElement("div");
+    bookCard.setAttribute("class", "card")
+
+    // create p element for title
+    const title = document.createElement("p");
+    title.textContent = `Title: ${book.title}`;
+
+    // create p element for author
+    const author = document.createElement("p");
+    author.textContent = `Author: ${book.author}`;
+
+    // create p element for book id
+    const bookId = document.createElement("p");
+    bookId.textContent = `ID: ${book.id}`;
+
+    // create element for read status
+    const readStatus = document.createElement("p");
+    readStatus.textContent = `Status: ${book.hasRead? "Read" : "Not read"}`;
+
+    // Add a button on each book’s display to remove the book from the library.
+    // create delete button
+    const deleteButton = document.createElement("button");
+    deleteButton.textContent = "Delete";
+    deleteButton.addEventListener("click", function(e) {
+        // remove book from library array
+        myLibrary = myLibrary.filter(function (libraryBook) {
+            return libraryBook.id !== book.id;
+        })
+
+        // remove book from display
+        this.parentElement.remove();
+    })
+
+    // Add a button on each book’s display to change its read status.
+    const readButton = document.createElement("button");
+    readButton.textContent = "Change read status";
+    readButton.addEventListener("click", () => {
+        book.toggleReadStatus();
+        readStatus.textContent = `Status: ${book.hasRead? "Read" : "Not read"}`;
+    })
+
+    // group buttons
+    const cardButtons = document.createElement("div");
+    cardButtons.setAttribute("class", "card-buttons");
+    cardButtons.appendChild(readButton);
+    cardButtons.appendChild(deleteButton);
+
+    // add elements to card
+    bookCard.appendChild(title);
+    bookCard.appendChild(author);
+    bookCard.appendChild(bookId);
+    bookCard.appendChild(readStatus);
+    bookCard.appendChild(cardButtons);
+
+    return bookCard;
+}
